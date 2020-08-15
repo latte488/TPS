9903db5894bbec8e523f3c92d77885283b693e12
latte488 <latte488@icloud.com>
2020-08-16 03:50:56 +0900

    7

--- Source/TPS/TPS.Build.cs
+++ Source/TPS/TPS.Build.cs
@@ -25,9 +25,8 @@ public class TPS : ModuleRules
 			PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libresolv.a"));
 			
 			PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libmysqlcppconn8-static.a"));
-
-			RuntimeDependencies.Add(Path.Combine(LibPath, "libmysqlcppconn8.so"));
 			*/
+			RuntimeDependencies.Add(Path.Combine(LibPath, "libmysqlcppconn8.so"));
 		}
 	}
 }

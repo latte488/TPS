b7fd60c0207c818cdf3837779387a3c51ffee2ad
latte488 <latte488@icloud.com>
2020-08-16 12:10:26 +0900

    7

--- Source/TPS/TPS.Build.cs
+++ Source/TPS/TPS.Build.cs
@@ -19,8 +19,8 @@ public class TPS : ModuleRules
 
 			PublicSystemLibraryPaths.Add(LibPath);
 			PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libcrypto.a"));
-			/*
 			PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libssl.a"));
+			/*
 			
 			PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libpthread.a"));
 			PublicAdditionalLibraries.Add(Path.Combine(LibPath, "libresolv.a"));
